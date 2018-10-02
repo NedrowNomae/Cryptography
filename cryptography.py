797b388de13bf8cb67455944eaf44d829c59132d
NedrowNomae <42844326+NedrowNomae@users.noreply.github.com>
2018-10-02 12:29:42 -0400

    Updated from Brython Server: 10/2/2018 12:29:41 PM

--- cryptography.py
+++ cryptography.py
@@ -11,59 +11,57 @@ See the detailed requirements at https://github.com/HHS-IntroProgramming/Cryptog
 """
 associations = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789 .,:;'\"/\\<>(){}[]-=_+?!"
 todo = input("Enter e to encrypt, d to decrypt, or q to quit: ")
-options = ["e","d","q"]
+options = ["e","d"]
 
 
 
-
-while todo not in options:
-    print("Did not understand command, try again.")
-    todo = input("Enter e to encrypt, d to decrypt, or q to quit: ")
-if todo == "e":
-    msg = input("Message: ")
-    key = input("Key: ")
-    msglist = list(msg)
-    k = len(key)
-    m = len(msg)
-    keylist = list(key)
-    while k<m:
-        keylist.extend(keylist)
-        k = len(keylist)
-    while k>m:
-        keylist.pop()
-        k = len(keylist)
-    #print(keylist)
-    #print(k)
-    encryptlist = ""
-    e = len(msglist)
-    for q in range(0,e):
-        w = associations[(associations.find(msglist[q]) + associations.find(keylist[q]))%85]
-        encryptlist = encryptlist + str(w)
-    print(encryptlist)
-if todo == "d":
-    msg = input("Message: ")
-    key = input("Key: ")
-    msglist = list(msg)
-    k = len(key)
-    m = len(msg)
-    keylist = list(key)
-    while k<m:
-        keylist.extend(keylist)
-        k = len(keylist)
-    while k>m:
-        keylist.pop()
-        k = len(keylist)
-    #print(keylist)
-    #print(k)
-    uncryptlist = ""
-    e = len(msglist)
-    for q in range(0,e):
-        w = associations[(associations.find(msglist[q]) - associations.find(keylist[q]))%85]
-        uncryptlist = uncryptlist + str(w)
-    print(uncryptlist)
-    todo = d
-if todo == "q":
-    print("Goodbye!")
+while todo not "q":
+    while todo not in options:
+        print("Did not understand command, try again.")
+        todo = input("Enter e to encrypt, d to decrypt, or q to quit: ")
+    if todo == "e":
+        msg = input("Message: ")
+        key = input("Key: ")
+        msglist = list(msg)
+        k = len(key)
+        m = len(msg)
+        keylist = list(key)
+        while k<m:
+            keylist.extend(keylist)
+            k = len(keylist)
+        while k>m:
+            keylist.pop()
+            k = len(keylist)
+        #print(keylist)
+        #print(k)
+        encryptlist = ""
+        e = len(msglist)
+        for q in range(0,e):
+            w = associations[(associations.find(msglist[q]) + associations.find(keylist[q]))%85]
+            encryptlist = encryptlist + str(w)
+        print(encryptlist)
+    if todo == "d":
+        msg = input("Message: ")
+        key = input("Key: ")
+        msglist = list(msg)
+        k = len(key)
+        m = len(msg)
+        keylist = list(key)
+        while k<m:
+            keylist.extend(keylist)
+            k = len(keylist)
+        while k>m:
+            keylist.pop()
+            k = len(keylist)
+        #print(keylist)
+        #print(k)
+        uncryptlist = ""
+        e = len(msglist)
+        for q in range(0,e):
+            w = associations[(associations.find(msglist[q]) - associations.find(keylist[q]))%85]
+            uncryptlist = uncryptlist + str(w)
+        print(uncryptlist)
+print("Goodbye!")
 
 
 #back = ""
