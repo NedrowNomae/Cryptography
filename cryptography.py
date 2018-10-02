72e33047c7389bd282dc27d7904fc91f7d9e83ca
NedrowNomae <42844326+NedrowNomae@users.noreply.github.com>
2018-10-02 12:36:33 -0400

    Updated from Brython Server: 10/2/2018 12:36:32 PM

--- cryptography.py
+++ cryptography.py
@@ -11,13 +11,13 @@ See the detailed requirements at https://github.com/HHS-IntroProgramming/Cryptog
 """
 associations = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789 .,:;'\"/\\<>(){}[]-=_+?!"
 todo = input("Enter e to encrypt, d to decrypt, or q to quit: ")
-options = ["e","d"]
+options = ["e","d","q"]
 a=1
 b=1
 
 
 while a == b:
-    while todo not in options:
+    if todo not in options:
         print("Did not understand command, try again.")
         todo = input("Enter e to encrypt, d to decrypt, or q to quit: ")
     if todo == "e":
